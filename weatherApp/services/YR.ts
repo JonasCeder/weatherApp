ac1468907307a18b2eb88cce3b459f0db45d0493
Jonas Cederkvist <jonas.cederkvist@aptitud.se>
2025-10-13 09:10:55 +0200

    Added precipitation to YR

--- weatherApp/services/YR.ts
+++ weatherApp/services/YR.ts
@@ -20,12 +20,20 @@ export const getYRWeatherData = async ({ lat, lon }: { lat: number, lon: number 
     if (symbolCode !== undefined) {
       symbolCode = symbolCode.replace('_day', '').replace('_night', '');
     }
+
+    const details = result.data.next_1_hours?.details ?? result.data.next_6_hours?.details;
+    if (details === undefined) return;
+
+    const probabilityOfPrecipitation = Math.ceil(details.probability_of_precipitation / 10) * 10;
     const weatherSymbol = getWeatherSymbolBySymbolCode(symbolCode);
     const date = new Date(result.time);
     const hour: Hour = {
       temp: result.data.instant.details.air_temperature,
       weatherSymbol: weatherSymbol,
-      date: date
+      date: date,
+      probabilityOfPrecipitation: probabilityOfPrecipitation,
+      precipitationAmountMax: details.precipitation_amount_max,
+      precipitationAmountMin: details.precipitation_amount_min
     };
     hours.push(hour)
   })
